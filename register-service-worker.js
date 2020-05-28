4ff3fd15a0f0b5165ca9c481f3dffa66f3e6333d
Xavier Carpentier <xcapetir@gmail.com>
2020-05-28 16:24:31 +0200

    Updates

---
+++ register-service-worker.js
@@ -0,0 +1,14 @@
+/* eslint-env browser */
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker
+      .register('/xcarpentier/rn-tourguide/expo-service-worker.js', { scope: '/xcarpentier/rn-tourguide/' })
+      .then(function(info) {
+        // console.info('Registered service-worker', info);
+      })
+      .catch(function(error) {
+        console.info('Failed to register service-worker', error);
+      });
+  });
+}
